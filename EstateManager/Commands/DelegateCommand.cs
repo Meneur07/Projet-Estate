d1cedc16a234c723e5d48d4f7841bf3e99363cb7
Cyril Gomes <cyril.gomes@etu.univ-lyon1.fr>
2020-02-28 09:24:07 +0100

    Initial Commit

---
+++ EstateManager/Commands/DelegateCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace EstateManager.Commands
+{
+    class DelegateCommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public DelegateCommand(Action execute) : this(execute, null) { }
+
+        public DelegateCommand(Action execute, Func<bool> canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException("execute");
+            _canExecute = canExecute;
+        }
+
+
+        public void OnCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute();
+        }
+
+        public virtual void Execute(object parameter)
+        {
+            if (CanExecute(parameter) && _execute != null)
+            {
+                _execute();
+            }
+        }
+    }
+}
---
+++ EstateManager/Commands/GenericDelegateCommand.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace EstateManager.Commands
+{
+    public class DelegateCommand<T> : ICommand
+    {
+        private readonly Action<T> _execute;
+        private readonly Func<T, bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public DelegateCommand(Action<T> execute) : this(execute, null) { }
+
+        public DelegateCommand(Action<T> execute, Func<T, bool> canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException("execute");
+            _canExecute = canExecute;
+        }
+
+
+        public void OnCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(GetParameter(parameter));
+        }
+
+        public virtual void Execute(object parameter)
+        {
+            if (CanExecute(parameter) && _execute != null)
+            {
+                T param = GetParameter(parameter);
+                _execute(param);
+            }
+        }
+
+        private T GetParameter(object parameter)
+        {
+            T param = default(T);
+            try
+            {
+                param = (T)parameter;
+            }
+            catch { }
+            return param;
+        }
+    }
+}
