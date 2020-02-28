a3b1acba4434c94d5c5ff548e9a7580731a9d1af
Jeje Roro <jerome.romy@etu.univ-lyon1.fr>
2020-02-28 12:13:22 +0100

    Remplissage classe Tag

--- EstateManager/Models/Tag.cs
+++ EstateManager/Models/Tag.cs
@@ -1,13 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EstateManager.Models
 {
-    class Tag
+    class Tag : ViewModels.BaseNotifyPropertyChanged
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id
+        {
+            get { return GetProperty<int>(); }
+            set { SetProperty(value); }
+        }
+
+
+        public string Label
+        {
+            get { return GetProperty<string>(); }
+            set { SetProperty(value); }
+        }
 
     }
 }
