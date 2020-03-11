86619dc63642618563469671fc2a1c2e24d142e9
Jeje Roro <jerome.romy@etu.univ-lyon1.fr>
2020-03-11 12:11:12 +0100

    Ajout de tous les viewModels + initialisation des dataContext

--- EstateManager/Views/EstatePage.xaml.cs
+++ EstateManager/Views/EstatePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EstateManager.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,7 @@ namespace EstateManager.Views
     {
         public EstatePage()
         {
+            DataContext = new EstateViewModel();
             InitializeComponent();
         }
     }
