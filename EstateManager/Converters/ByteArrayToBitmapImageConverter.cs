e04b89e5dd55dd22a1eaadb0a71cacf8b5e489ae
Jeje Roro <jerome.romy@etu.univ-lyon1.fr>
2020-03-14 15:50:04 +0100

    Ajout du converter byte[] to Bitmap

---
+++ EstateManager/Converters/ByteArrayToBitmapImageConverter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace EstateManager.Converters
+{
+    public class ByteArryaToBitmapImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return null;
+            if (value.GetType() != typeof(byte[])) return null;
+
+            return ByteArrayToBitmapImage((byte[])value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return "";
+            if (value.GetType() != typeof(BitmapImage)) return "";
+            
+            return BitmapImageToByteArray((BitmapImage)value);
+        }
+
+        public static BitmapImage ByteArrayToBitmapImage(byte[] byteArray)
+        {
+            using (MemoryStream stream = new MemoryStream(byteArray))
+            {
+                BitmapImage image = new BitmapImage();
+                image.BeginInit();
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.StreamSource = stream;
+                image.EndInit();
+                image.Freeze();
+                return image;
+            }
+        }
+
+        public static byte[] BitmapImageToByteArray(BitmapImage image)
+        {
+            if (image == null) return null;
+
+            using (MemoryStream stream = new MemoryStream())
+            {
+                PngBitmapEncoder encoder = new PngBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(image));
+                encoder.Save(stream);
+                return stream.ToArray();
+            }
+        }
+    }
+}
