9ae2f0e8f5df38885485721d239107d0484c8dcd
Jeje Roro <jerome.romy@etu.univ-lyon1.fr>
2020-03-15 10:57:56 +0100

    Ajout du bindage de toutes les données de la fenetre addTransaction (sauf les boutons et l'image)

--- EstateManager/ViewModels/AddTransacViewModel.cs
+++ EstateManager/ViewModels/AddTransacViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EstateManager.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,29 @@ namespace EstateManager.ViewModels
 {
     class AddTransacViewModel
     {
+        // Transaction part
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime PubDate { get; set; }
+        public DateTime TransacDate { get; set; }
+        public TypeTransaction TransacType { get; set; }
+        public double Price { get; set; }
+        public double Fees { get; set; }
+        public int PropId { get; set; }
+        public int CliId { get; set; }
 
+
+        //Estate part
+        public string Reference { get; set; }
+        public TypeEstate EstateType { get; set; }
+        public int FloorCount { get; set; }
+        public int BathroomCount { get; set; }
+        public float Surface { get; set; }
+        public string Address { get; set; }
+        public string ZipCode { get; set; }
+        public int FloorNumber { get; set; }
+        public int CarbonFootPrint { get; set; }
+        public int EnergeticPerformance { get; set; }
+        public int ComercialId { get; set; }
     }
 }
