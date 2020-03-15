2eb7aaff3d7a4f7c822c3e7d03f647672357c9e1
Jeje Roro <jerome.romy@etu.univ-lyon1.fr>
2020-03-15 12:40:32 +0100

    Ajout du style avec none pour la fenetre add transaction

--- EstateManager/Views/AddTransaction.xaml.cs
+++ EstateManager/Views/AddTransaction.xaml.cs
@@ -1,16 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
+using System.Windows.Forms;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace EstateManager.Views
 {
@@ -23,6 +13,43 @@ namespace EstateManager.Views
         {
             DataContext = new ViewModels.AddTransacViewModel();
             InitializeComponent();
+        }
+
+        private void Rectangle_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+
+
+        private void Ellipse_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Maximize(object sender, MouseButtonEventArgs e)
+        {
+            if (Width == Screen.PrimaryScreen.WorkingArea.Width && Height == Screen.PrimaryScreen.WorkingArea.Height)
+            {
+
+                Width = 800;
+                Height = 500;
+            }
+            else
+            {
+
+
+                Left = Top = 0;
+
+                Width = Screen.PrimaryScreen.WorkingArea.Width;
+                Height = Screen.PrimaryScreen.WorkingArea.Height;
+            }
+
+        }
+
+        private void Minimize(object sender, MouseButtonEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
         }
     }
 }
