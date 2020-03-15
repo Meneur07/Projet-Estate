18c33897f895bf1f7e8d7fab4c60b7c848e17b37
Jeje Roro <jerome.romy@etu.univ-lyon1.fr>
2020-03-15 12:03:36 +0100

    Ajout de la prévisualisation de la photo dans addTransaction

--- EstateManager/ViewModels/AddTransacViewModel.cs
+++ EstateManager/ViewModels/AddTransacViewModel.cs
@@ -1,14 +1,16 @@
-﻿using EstateManager.Models;
+﻿using EstateManager.DataAccess;
+using EstateManager.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+using System.IO;
 
 namespace EstateManager.ViewModels
 {
-    class AddTransacViewModel
+    class AddTransacViewModel : BaseNotifyPropertyChanged
     {
+        private EstateManagerContext dbContext = EstateManagerContext.Current;
+
         // Transaction part
         public string Title { get; set; }
         public string Description { get; set; }
@@ -33,5 +35,47 @@ namespace EstateManager.ViewModels
         public int CarbonFootPrint { get; set; }
         public int EnergeticPerformance { get; set; }
         public int ComercialId { get; set; }
+        public byte[] Photo { get; set; }
+
+        public string ImagePath
+        {
+            get { return GetProperty<string>(); }
+            set { SetProperty<string>(value); }
+        }
+
+
+        public ICommand AddCommand
+        {
+            get
+            {
+                return new Commands.DelegateCommand(clickAdd);
+            }
+        }
+
+        void clickAdd()
+        {
+            //Ajouter la transac et l'estate dans la BD
+        }
+
+        public ICommand PickImageCommand
+        {
+            get
+            {
+                return new Commands.DelegateCommand(clickPickImage);
+            }
+        }
+
+        void clickPickImage()
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+
+            bool? result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                ImagePath = dlg.FileName;
+            }
+        }
     }
 }
