0863d578f97213b28e7ebd19c31334380e9a77d2
Cyril Gomes <cyril.gomes@etu.univ-lyon1.fr>
2020-03-25 17:27:15 +0100

    Ajout des filtres

--- EstateManager/ViewModels/EstateViewModel.cs
+++ EstateManager/ViewModels/EstateViewModel.cs
@@ -5,11 +5,43 @@ using System.Windows;
 using System.Windows.Input;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 namespace EstateManager.ViewModels
 {
     class EstateViewModel : BaseNotifyPropertyChanged
     {
+
+
+
+
+
+
+
+
+        public Models.TypeEstate EstateTypeFilter { get; set; }
+        public Models.TypeTransaction TransactionTypeFilter { get; set; }
+        public string LocalisationFilter { get; set; }
+        public float MaxBudgetFilter { get; set; }
+        public float MinSurfaceFilter { get; set; }
+        public int FloorsCountFilter { get; set; }
+        public int FloorNumberFilter { get; set; }
+        public int RoomsCountFilter { get; set; }
+        public int BathroomsCountFilter { get; set; }
+
+
+        public bool ShouldApplyFilter
+        {
+            get { return GetProperty<bool>(); }
+            set {
+                SetProperty<bool>(value);
+                    updateContent();
+                    }
+        }
+
+
+
+
 
 
         public ObservableCollection<Transaction> Transactions
@@ -32,9 +64,26 @@ namespace EstateManager.ViewModels
 
         void updateContent()
         {
+
             Transactions.Clear();
-            var transList = dbContext.Transactions.Include(b => b.Estate).ThenInclude(est => est.Photos).ToList();
-            foreach (var trans in transList)
+            var request = dbContext.Transactions.Include(b => b.Estate).ThenInclude(est => est.Photos);
+            List<Transaction> translist = request.ToList();
+            if (ShouldApplyFilter)
+            {
+
+                translist = request.Where(t => t.Price < MaxBudgetFilter || MaxBudgetFilter == 0).
+                Where(b => b.Estate.Surface > MinSurfaceFilter || MinSurfaceFilter == 0).
+                Where(t => t.Type == TransactionTypeFilter && t.Estate.Type == EstateTypeFilter).
+                Where(t => t.Estate.FloorCount == FloorsCountFilter || FloorsCountFilter == 0).
+                Where(t => t.Estate.RoomsCount == RoomsCountFilter || RoomsCountFilter == 0).
+                Where(t => t.Estate.BathroomCount == BathroomsCountFilter || BathroomsCountFilter == 0).
+                Where(t => t.Estate.FloorNumber == FloorNumberFilter || FloorNumberFilter == 0).ToList();
+            }
+
+
+
+
+            foreach (var trans in translist)
             {
                 Transactions.Add(trans);
             }
@@ -52,6 +101,40 @@ namespace EstateManager.ViewModels
         {
             var windowAdd = new Views.AddTransaction();
             windowAdd.ShowDialog();
+            updateContent();
+        }
+
+
+
+        public ICommand ApplyFilterCommand
+        {
+            get
+            {
+                return new Commands.DelegateCommand(ApplyFilters);
+            }
+        }
+
+        void ApplyFilters()
+        {
+            windowFilter.Close();
+        }
+
+        public ICommand FilterCommand
+        {
+            get
+            {
+                return new Commands.DelegateCommand(ClickFilter);
+            }
+        }
+
+        Views.FilterPopup windowFilter;
+        void ClickFilter()
+        {
+            windowFilter = new Views.FilterPopup
+            {
+                DataContext = this
+            };
+            windowFilter.ShowDialog();
             updateContent();
         }
 
@@ -81,7 +164,9 @@ namespace EstateManager.ViewModels
 
         void clickModify(int idEstate)
         {
-            MessageBox.Show("Je veux modify l'estate avec l'id : " + idEstate);
+            ModifyTransaction mt = new ModifyTransaction(idEstate);
+            mt.ShowDialog();
+            updateContent();
         }
 
         public ICommand DetailsCommand
