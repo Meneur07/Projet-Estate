1903686fca6614e1d06c288a4cb45c848e515014
Cyril Gomes <cyril.gomes@etu.univ-lyon1.fr>
2020-03-30 17:44:27 +0200

    Ajout Map

--- EstateManager/ViewModels/AppointmentViewModel.cs
+++ EstateManager/ViewModels/AppointmentViewModel.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using Microsoft.Maps.MapControl.WPF;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace EstateManager.ViewModels
 {
     class AppointmentViewModel
     {
+
     }
 }
