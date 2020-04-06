ddaff4b60e79d468466b1345bbf87337d3d3f635
Cyril Gomes <cyril.gomes@etu.univ-lyon1.fr>
2020-04-06 10:54:20 +0200

    Ajout points sur la map

---
+++ EstateManager/Models/MapPoint.cs
@@ -0,0 +1,29 @@
+﻿using EstateManager.ViewModels;
+using Microsoft.Maps.MapControl.WPF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstateManager.Models
+{
+    class MapPoint : BaseNotifyPropertyChanged
+    {
+
+        public double Latitude
+        {
+            get { return GetProperty<double>(); }
+            set { SetProperty<double>(value); }
+        }
+
+        public double Longitude
+        {
+            get { return GetProperty<double>(); }
+            set { SetProperty<double>(value); }
+        }
+
+        public Location location { get { return new Location(Latitude, Longitude); } }
+
+    }
+}
